db72d6484bb684f87228a7ea48aaf2af4c0a3ec5
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2020-12-21 18:59:15 +0100

    Brackets

--- commands/up.js
+++ commands/up.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'up',
     description: "this is a ping command!",
     execute(message, commandArgs, command, Tags, MessageEmbed, Discord, client) {
-        if (Math.random < 0.05) {
+        if (Math.random() < 0.05) {
             message.channel.send('\*\*YES\*\*')
             setTimeout(() => {
                 message.channel.send('\*\*JK no ofc not\*\*')
