c6092ff099e8b37e6b4b0c0f885034135f08cc69
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2020-12-24 11:59:08 +0100

    Guild member

--- commands/whitelist.js
+++ commands/whitelist.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: 'whitelist',
     description: "Me want whitelist",
-    execute(message, commandArgs, command, Tags, MessageEmbed, Discord, client, config) {
+    execute(message, commandArgs, command, Tags, MessageEmbed, Discord, client, config, Guild) {
         const channel = client.channels.cache.get(config.whitechannel)
-        const author = message.author.name
+        const author = Guild.member(message.author)
         channel.send(`${author} Wants to be whitelisted, Their IGN: ${commandArgs}`)
     }
 }
