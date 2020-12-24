1b3659d3a7841a39fa8bd7ee3a7de8ce4f7de638
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2020-12-24 11:50:26 +0100

    cmon whitelist

--- Create.js
+++ Create.js
@@ -94,8 +94,8 @@ client.on('message', async message => {
 			client.commands.get('reactionrole').execute(message, commandArgs, command, Tags, MessageEmbed, Discord, client)
 		} else if (command == 'up') {
 			client.commands.get('up').execute(message, commandArgs, command, Tags, MessageEmbed, Discord, client)
-		} else if (command == 'up') {
-			client.commands.get('up').execute(message, commandArgs, command, Tags, MessageEmbed, Discord, client)
+		} else if (command == 'whitelist') {
+			client.commands.get('whitelist').execute(message, commandArgs, command, Tags, MessageEmbed, Discord, client)
 		} else {
 			NoCommand.setTitle('Command not found.');
 			NoCommand.addField(`The Command, ${command}, is not in use by this bot.`, `Think the command should be used? DM Hyperbuilder`)
