71d74c761275b0cacfbb1f69c47eb8b418544ba2
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2020-12-24 11:53:49 +0100

    Config fix

--- commands/whitelist.js
+++ commands/whitelist.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'whitelist',
     description: "Me want whitelist",
-    execute(message, commandArgs, command, Tags, MessageEmbed, Discord, client) {
+    execute(message, commandArgs, command, Tags, MessageEmbed, Discord, client, config) {
         const channel = client.channels.cache.get(config.whitechannel)
         const author = message.author.name
         channel.send(`${author} Wants to be whitelisted, Their IGN: ${commandArgs}`)
