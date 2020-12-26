ac4ab5ba7114f510182862c4ea9ef5ac19531c23
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2020-12-26 18:26:20 +0100

    status

--- commands/up.js
+++ commands/up.js
@@ -4,7 +4,7 @@ module.exports = {
     async execute(message, commandArgs, command, Tags, MessageEmbed, Discord, client, config, rcon) {
         rcon.connect().then(() => {
             console.log('Connected!');
-            return rcon.send('/list');
+            return rcon.send('/status');
         }).then(response => {
             console.log(response); // Print the server response to console
             return rcon.disconnect();
