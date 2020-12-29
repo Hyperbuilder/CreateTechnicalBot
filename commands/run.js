ed32d0381b54736c9424c3f45bcc905f197c9738
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2020-12-29 11:18:35 +0100

    client

--- commands/run.js
+++ commands/run.js
@@ -1,14 +1,17 @@
 module.exports = {
     name: 'run',
     description: "Runcommands",
-    execute(message, commandArgs, command, Tags, MessageEmbed, Discord, client, rconC) {
+    execute(message, commandArgs, command, Tags, MessageEmbed, Discord) {
+        const util = require('minecraft-server-util');
+
+        const client = new util.RCON(config.ip, { port: 25575, enableSRV: true, timeout: 5000, password: config.RconPass });
         client.on('output', (message) => console.log(message));
 
-        rconC.connect()
+        client.connect()
             .then(async () => {
-                await rconC.run(`${commandArgs}`);
+                await client.run(`${commandArgs}`);
 
-                rconC.close();
+                client.close();
             })
             .catch((error) => {
                 message.channel.send(error)
