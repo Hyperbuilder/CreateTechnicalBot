7ab47dce75cfc3a77cf4d66300eb6f2bb970a6ca
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2020-12-29 11:21:03 +0100

    Update run.js

--- commands/run.js
+++ commands/run.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(message, commandArgs, command, Tags, MessageEmbed, Discord, config) {
         const util = require('minecraft-server-util');
 
-        const client = new util.RCON(config.ip, { port: 25575, enableSRV: true, timeout: 5000, password: config.RconPass });
+        const client = new util.RCON(`${config.ip}`, { port: 25575, enableSRV: true, timeout: 5000, password: config.RconPass });
         client.on('output', (message) => console.log(message));
 
         client.connect()
