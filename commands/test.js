4abef12d1ed36cbd0e4e89f01fed643fa45accd1
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2020-12-31 19:13:24 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'test',
     description: "this is a test command!",
     execute(message, commandArgs, command, Tags, MessageEmbed, Discord, client){
-        message.channel.send(`Running \nlocaltime: ${Date}`)
+        message.channel.send(`Running \nlocaltime: ${Date()}`)
     }
 }
