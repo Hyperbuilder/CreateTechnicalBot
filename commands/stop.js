2ba3e96f577a029d4948cbd6ef7940c02af1eae8
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2020-12-31 19:03:53 +0100

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -2,9 +2,8 @@ module.exports = {
     name: 'stop',
     description: "this is a stop command!",
     execute(message, commandArgs, command, Tags, MessageEmbed, Discord, client, BotOwner) {
-        const attachment = new MessageAttachment('https://tenor.com/8pOE.gif');
         if (!BotOwner) {
-            message.channel.send(attachment)
+            message.channel.send('https://tenor.com/8pOE.gif')
         } else {
             message.channel.send('Shutting down...').then(m => {
                 client.destroy();
