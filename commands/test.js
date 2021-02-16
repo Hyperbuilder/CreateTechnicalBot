04e6fbc2276ec4eb2e5b426240c3cdf1437ee4cf
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2021-02-16 17:36:57 +0100

    More updates to Test

--- commands/test.js
+++ commands/test.js
@@ -2,8 +2,11 @@ module.exports = {
     name: 'test',
     description: "this is a test command!",
     execute(message, commandArgs, command, Tags, MessageEmbed, Discord, client){
+        const config = require("../config.json")
         const date = new Date();
         const uptime = (Math.floor(process.uptime()) / 60) / 60
         message.channel.send(`Running \nlocaltime: ${date}\nUptime: ${uptime}`)
+        message.channel.send(`Stored Server IP: ${config.IP}`)
+        
     }
 }
