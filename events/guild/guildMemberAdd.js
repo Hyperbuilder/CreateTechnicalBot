7bd393bc3f1b515b1c6d87709cdd6f8c9eb6032e
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2021-04-04 12:21:22 +0200

    Update guildMemberAdd.js

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,5 +1,4 @@
 const ProfileSchema = require("../../ProfileSchema");
-const MessageEmbed = require("discord.js")
 
 module.exports = async (client, discord, member) => {
     let profile = await profileModel.create({
@@ -9,9 +8,11 @@ module.exports = async (client, discord, member) => {
         bank: 0,
     });
     profile.save();
-}
 
-const Welcome = new MessageEmbed()
-Welcome.setTitle(`Welcome to CT`)
-Welcome.addField(`Welcome To Create Technical`, `Hey ${member}, we're excited to have you join our community.\nPlease put your application in the Applications channel. When you recieve your Member role use /whitelist <MinecraftPlayerName> in Bot-channel-and-shaming. \nHave Fun!`)
-Guildmember.guild.channels.cache.get('737425690877493309').send({ embed: Welcome });
+
+    const Welcome = new MessageEmbed()
+    Welcome.setTitle(`Welcome to CT`)
+    Welcome.addField(`Welcome To Create Technical`, `Hey ${member}, we're excited to have you join our community.\nPlease put your application in the Applications channel. When you recieve your Member role use /whitelist <MinecraftPlayerName> in Bot-channel-and-shaming. \nHave Fun!`)
+    Guildmember.guild.channels.cache.get('737425690877493309').send({ embed: Welcome });
+
+}
