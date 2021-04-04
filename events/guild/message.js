26e5e8e7f9671fa55d662dcaa3e196aafdd84e2b
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2021-04-04 12:08:00 +0200

    Update Apply System

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,7 +1,38 @@
 module.exports = (Discord, client, message) => {
     const prefix = '!'
+    const strings = require("../../strings/strings.js")
+    const actions = require("../../")
 
     if (!message.content.startsWith(prefix) || message.author.bot) return
+
+    let hasRanCommand = false;
+    const strLen = str.length;
+
+    if (msg.content.substr(0, strLen) === prefix) {
+        const command = msg.content.substr(strLen);
+
+        hasRanCommand = true;
+
+        if (!actions[command])
+            msg.reply(strings.unknownCommand);
+
+        try {
+            actions[command](msg);
+        } catch (e) {
+            msg.reply(strings.error);
+            console.error(e);
+        }
+    }
+
+    if (!message.cintent.startsWith(prefix) && msg.channel.type === "dm") {
+        try {
+            actions.directMessage(msg);
+        } catch (e) {
+            msg.reply(strings.error);
+            console.error(e);
+        }
+        return
+    }
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const cmd = args.shift().toLowerCase();
