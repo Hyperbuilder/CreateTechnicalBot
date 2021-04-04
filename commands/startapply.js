e8cf8ea5c5332705aa1502beb0281fb4063000c8
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2021-04-04 14:56:19 +0200

    Update startapply.js

--- commands/startapply.js
+++ commands/startapply.js
@@ -1,18 +1,18 @@
 module.exports = {
     name: 'startapply',
-    description: "this makes the bot say pong",
+    description: "",
     async execute(client, message, args, Discord) {
         // put the channel id in the brackets bellow
         const applychannel = '828237716084359189'
-        const memberRole = message.guild.roles.cache.find(role => role.name === "member");
 
         const memberEmoji = '🔵';
 
         let embed = new Discord.MessageEmbed()
             .setTitle('Apply Here')
-            .setDescription(`apply here using`, `🔵`);
+            .setDescription(`apply here using: 🔵`);
 
-        let messageEmbed = await applychannel.send(embed).then(messageEmbed.react(memberEmoji));
+        let messageEmbed = await message.channel.send(embed)
+        messageEmbed.react(memberEmoji)
 
         client.on('messageReactionAdd', async (reaction, user) => {
             if (reaction.message.partial) await reaction.message.fetch();
@@ -21,8 +21,7 @@ module.exports = {
             if (!reaction.message.guild) return;
 
             if (reaction.emoji.name === memberEmoji) {
-                await reaction.message.guild.members.cache.get(user.id).roles.add(memberRole)
-
+                await reaction.message.guild.members.cache.get(user.id)
             } else {
                 return;
             }
