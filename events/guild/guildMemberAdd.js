35fdae1f861fdb154640499ec85186dfe358dc0b
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2021-05-21 21:17:31 +0200

    Welcome message update

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -12,7 +12,7 @@ module.exports = async (client, discord, member) => {
 
     const Welcome = new MessageEmbed()
     Welcome.setTitle(`Welcome to CT`)
-    Welcome.addField(`Welcome To Create Technical`, `Hey ${member}, we're excited to have you join our community.\nPlease put your application in the Applications channel. When you recieve your Member role use /whitelist <MinecraftPlayerName> in Bot-channel-and-shaming. \nHave Fun!`)
-    Guildmember.guild.channels.cache.get('737425690877493309').send({ embed: Welcome });
+    Welcome.addField(`Welcome To Create Technical`, `Hey ${member}, we're excited to have you join our community.\nPlease start an application form in <#839859704075517973> channel. When you recieve your Member role use /whitelist <MinecraftPlayerName> in <#778816644284940328>. \nHave Fun!`)
+    member.guild.channels.cache.get('737425690877493309').send({ embed: Welcome });
 
 }
