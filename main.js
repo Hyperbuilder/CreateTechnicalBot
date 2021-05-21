ba717dcb4866aa70a2c80e5e6678afa9f8c49190
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2021-05-21 21:43:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] })
 const config = require('./config.json');
-const error = require("./utils/error");
+const error = require("./utils/error.js");
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
