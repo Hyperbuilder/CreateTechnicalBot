cf6cc73c33718f981146c2c6bf81228316ab2502
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2021-05-22 12:09:26 +0200

    Disable profiles
    Enable again when needed

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -2,21 +2,21 @@ const mongo = require("../../mongo");
 const ProfileSchema = require("../../Schema/ProfileSchema");
 
 module.exports = async (client, discord, member) => {
-    await mongo().then(mongoose => {
-        try {
-            let profile = await ProfileSchema.create({
-                userID: member.id,
-                guildID: member.guild.id,
-                brass: 1000,
-                bank: 0,
-                xp: 0,
-                level: 1
-            });
-            profile.save();
-        } finally {
-            mongoose.connection.stop()
-        }
-    })
+    // await mongo().then(mongoose => {
+    //     try {
+    //         let profile = await ProfileSchema.create({
+    //             userID: member.id,
+    //             guildID: member.guild.id,
+    //             brass: 1000,
+    //             bank: 0,
+    //             xp: 0,
+    //             level: 1
+    //         });
+    //         profile.save();
+    //     } finally {
+    //         mongoose.connection.stop()
+    //     }
+    // })
     const Welcome = new MessageEmbed()
     Welcome.setTitle(`Welcome to CT`)
     Welcome.addField(`Welcome To Create Technical`, `Hey ${member}, we're excited to have you join our community.\nPlease start an application form in <#839859704075517973> channel. When you recieve your Member role use /whitelist <MinecraftPlayerName> in <#778816644284940328>. \nHave Fun!`)
