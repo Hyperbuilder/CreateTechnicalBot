95cf0c942675833ec25a81d46a74ab2613cf58fb
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2021-05-22 18:04:00 +0200

    Fix Welcome message

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,10 +1,6 @@
 const mongo = require("../../mongo")
-const actions = require("../../actions")
 const chalk = require('chalk');
 
-const error = chalk.black.bgRedBright;
-const warning = chalk.keyword('orange');
-const log = chalk.blueBright;
 const info = chalk.cyanBright;
 const success = chalk.keyword('lime');
 
@@ -12,13 +8,13 @@ const success = chalk.keyword('lime');
 module.exports = async (Discord, client) => {
     console.log(success(`\nCT Test + Gamble bot is running`));
 
-    // await mongo().then(mongoose => {
-    //     try {
-    //         console.log(info(`Connected to MongoDB`))
-    //     } finally {
-    //         mongoose.connection.close()
-    //     }
-    // })
+    await mongo().then(mongoose => {
+        try {
+            console.log(info(`Connected to MongoDB`))
+        } finally {
+            mongoose.connection.close()
+        }
+    })
 
     client.user.setActivity("Create Technical Discord", { type: 'WATCHING' })
 }
--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -2,6 +2,9 @@ const mongo = require("../../mongo");
 const ProfileSchema = require("../../Schema/ProfileSchema");
 
 module.exports = async (client, Discord, member) => {
+
+    const welcomeembed = new Discord.MessageEmbed()
+
     // await mongo().then(mongoose => {
     //     try {
     //         let profile = await ProfileSchema.create({
@@ -17,9 +20,10 @@ module.exports = async (client, Discord, member) => {
     //         mongoose.connection.stop()
     //     }
     // })
-    const Welcome = new Discord.MessageEmbed()
-    Welcome.setTitle(`Welcome to CT`)
-    Welcome.addField(`Welcome To Create Technical`, `Hey ${member}, we're excited to have you join our community.\nPlease start an application form in <#839859704075517973> channel. When you recieve your Member role use /whitelist <MinecraftPlayerName> in <#778816644284940328>. \nHave Fun!`)
-    member.guild.channels.cache.get('737425690877493309').send({ embed: Welcome });
+
+
+    welcomeembed.setTitle(`Welcome to CT`)
+    welcomeembed.addField(`Welcome To Create Technical`, `Hey ${member}, we're excited to have you join our community.\nPlease start an application form in <#839859704075517973> channel. When you recieve your Member role use /whitelist <MinecraftPlayerName> in <#778816644284940328>. \nHave Fun!`)
+    member.guild.channels.cache.get('737425690877493309').send({ embed: welcomeembed });
 
 }
